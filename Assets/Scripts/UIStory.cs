f172175546cd598e4a900be1d04c554f6b33e4eb
Tim Moss <tmoss726@hotmail.com>
2016-01-30 17:22:12 -0500

    Starting story script

---
+++ Assets/Scripts/UIStory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIStory : MonoBehaviour 
+{
+	private Text storyText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		storyText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+	}
+}
