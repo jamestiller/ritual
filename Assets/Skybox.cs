ea7eab2861910fde8fe5073250e540f56677291c
badbabyjames <badbabygames@gmail.com>
2016-01-30 17:06:44 -0500

    Skybox height prefab

---
+++ Assets/Skybox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skybox : MonoBehaviour
+{
+
+	private Transform playerTransform;
+	public Material[] Skyboxes;
+	public float maxHeight;
+
+	// Use this for initialization
+	void Start ()
+	{
+		var player = GameObject.FindGameObjectWithTag ("Player");
+		playerTransform = player.GetComponent<Transform> ();
+		RenderSettings.skybox = Skyboxes [0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		RenderSettings.skybox = Skyboxes [Mathf.RoundToInt (playerTransform.position.y / (maxHeight / 15))];
+	}
+}
