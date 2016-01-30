86596925ee06e862467f4311c1dc3dc23aaefc81
badbabyjames <badbabygames@gmail.com>
2016-01-30 15:32:19 -0500

    Adding Sound Trigger Prefab

---
+++ Assets/Triggers/Scripts/SoundTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(BoxCollider))]
+public class SoundTrigger : MonoBehaviour
+{
+
+	public GameObject[] SoundSources;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	void OnTriggerEnter (Collider collision)
+	{
+		foreach (GameObject source in SoundSources) {
+			AudioSource audio = source.GetComponent<AudioSource> ();
+			audio.Play ();
+		}
+	}
+}
