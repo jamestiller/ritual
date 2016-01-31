02422a207aa5808c0106a2d2151e96eae9727833
badbabyjames <badbabygames@gmail.com>
2016-01-30 20:55:27 -0500

    Triggers and Ngui stuff

---
+++ Assets/Triggers/Scripts/StoryWriterScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryWriterScript : MonoBehaviour
+{
+	[TextArea (3, 10)]
+	public string Story;
+
+	public float DisplayTime;
+
+	private Text text;
+	// Use this for initialization
+	void Start ()
+	{
+		text = FindObjectOfType<Canvas> ().transform.FindChild ("Text").gameObject.GetComponent<Text> ();
+	}
+
+	public IEnumerator Write ()
+	{
+		Debug.Log ("Write!");
+		text.text = Story;
+		yield return new WaitForSeconds (DisplayTime);
+		text.text = "";
+	}
+}
---
+++ Assets/Triggers/Scripts/TextTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTrigger : MonoBehaviour
+{
+
+	public GameObject StoryWriter;
+
+	private bool triggered;
+
+	void Start ()
+	{
+		triggered = false;
+	}
+
+	public void OnTriggerEnter (Collider collider)
+	{
+		Debug.Log ("Trigger!");
+		StoryWriterScript writer = (StoryWriterScript)StoryWriter.GetComponent<StoryWriterScript> ();
+		Debug.Log (writer);
+		if (!triggered) {
+			StartCoroutine (writer.Write ());
+		}
+		triggered = true;
+	}
+}
